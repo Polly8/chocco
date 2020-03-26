03a5123484e363044f80d23cbcd40f3a377809f9
Polina Sovershennaya <perfectwlf@yandex.ru>
2020-03-26 15:27:00 +0300

    correct menu overflow

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 //////////-Humburger menu-//////////
 
 const humburger = document.querySelector(".header__humburger-link");
-const menu = document.querySelector(".header__menu");
-const menuComputed = getComputedStyle(menu);
-const closeMenu = document.querySelector(".menu-close");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeMenu = document.querySelector(".mobile-menu__close");
+
+
 
 humburger.addEventListener("click", function(event){
     event.preventDefault();
-    
-    menu.style.display = "flex";
-    closeMenu.style.display = "block";
-    
+
+    mobileMenu.classList.remove("mobile-menu--closed");
+    document.body.style.overflow = "hidden";   
 });
 
-closeMenu.addEventListener("click", function(){
-    menu.style.display = "none";
-    closeMenu.style.display = "none";
+closeMenu.addEventListener("click", function(event){
+    event.stopPropagation();
+    
+    mobileMenu.classList.add("mobile-menu--closed");
+    document.body.style.overflow = "visible";
 })
+
 
 
 //////////-Bars consist-//////////
@@ -71,3 +74,32 @@ accoMenu.addEventListener("click", function(){
         menuItem[i].classList.remove("menu__item--opened");
     }
 })
+
+
+//////////-Team accordion-//////////
+
+const teamItem = document.querySelectorAll(".team__item");
+const linkTeam = document.querySelectorAll(".team__name");
+const linkLength = linkTeam.length;
+const teamLength = teamItem.length;
+
+for (i = 0; i < linkLength; i++){
+
+    linkTeam[i].addEventListener("click", function(event){
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (this.parentNode.classList.contains("team__item--active")){
+            
+            this.parentNode.classList.remove("team__item--active");
+        }else{
+            
+            for (i = 0; i < teamLength; i++){
+
+                linkTeam[i].parentNode.classList.remove("team__item--active");
+            }
+
+            this.parentNode.classList.add("team__item--active");
+        }        
+    })
+}
